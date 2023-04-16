a8e2db2d91b949cd9d32a67372ccc6b44a9a94db
Bisma Fajar <120157054+bismafajar816@users.noreply.github.com>
2023-04-16 23:20:49 +0500

    Add files via upload

---
+++ admin/admin/obj/Debug/Credentials.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace admin
+{
+    public class Credentials
+    {
+        public string name;
+        public string password;
+        public string role;
+    }
+}
